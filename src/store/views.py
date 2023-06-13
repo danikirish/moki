ec4d0e39242b0f6f0a6862c39c590f419a81f890
Daniyar Irishev <daniyar@zevero.earth>
2023-06-13 11:26:13 +0600

    Add base templates

--- src/store/views.py
+++ src/store/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
+from store.models import Product
+
+
 # Create your views here.
+def index(request):
+    """View function for home page of site."""
+
+    num_products = Product.objects.all().count()
+
+    num_instances_available = Product.objects.filter(stock__gt=0).count()
+
+    context = {
+        "num_products": num_products,
+        "num_instances_available": num_instances_available,
+    }
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(request, "index.html", context=context)
